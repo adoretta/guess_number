7fd4c52370036e8ae44b44dfd1cd59a706bf872a
Etta Chen <10102040@gs.ncku.edu.tw>
2019-07-27 14:37:58 +0800

    user_input

--- guess_number.py
+++ guess_number.py
@@ -1,11 +1,14 @@
 import random
-r = random.randint(1, 100)
+start = int(input('請決定隨機數字開始值: '))
+end = int(input('請決定隨機數字結束值: '))
+r =random.randint(start, end)
 count = 0
 while True:
 	count += 1
 	number = int(input('請輸入數字: '))
 	if number == r:
 		print('終於猜對了')
+		print('這是你猜的第', count, '次')
 		break
 	elif number > r :
 		print('比答案大')
